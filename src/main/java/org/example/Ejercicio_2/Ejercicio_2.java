df101b3e8ca7d94fb9f7cecbb189718e9349ea6d
Guido <guidoc1997@gmail.com>
2022-09-26 19:22:30 -0300

    Cambio al ejercicio 2

--- src/main/java/org/example/Ejercicio_2/Ejercicio_2.java
+++ src/main/java/org/example/Ejercicio_2/Ejercicio_2.java
@@ -6,9 +6,7 @@ public class Ejercicio_2 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         System.out.println("Ingrese 2 numeros");
-        int numero1 = read.nextInt();
         int numero2 = read.nextInt();
-        int suma = numero1 + numero2;
-        System.out.print("La sumatoria es: "+suma);
+        System.out.print("La sumatoria es: "+(read.nextInt() + numero2));
     }
 }
