0bd2467abed55e7804acca982d45b51d6f4c6486
Guido <guidoc1997@gmail.com>
2022-09-26 19:45:50 -0300

    Resolucion ejercicio 3

---
+++ src/main/java/org/example/Ejercicio_3/Ejercicio_3.java
@@ -0,0 +1,12 @@
+package org.example.Ejercicio_3;
+
+import java.util.Scanner;
+
+public class Ejercicio_3 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese su nombre");
+        String name = read.next();
+        System.out.print(name);
+    }
+}
